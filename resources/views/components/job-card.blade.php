edf63f879b61691ad78ad466a05c8070b6e28774
Conor <conor@visibleone.com>
2023-06-26 17:24:12 +0800

    Create Job Page and Job Card Link :bricks:

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,23 @@
+<x-card class="mb-4 last:mb-0 p-4">
+    <div class="flex justify-between items-center mb-4">
+        <h2 class="text-lg font-medium">{{ $job->title }}</h2>
+        <div class="text-slate-500">
+            ${{ number_format($job->salary) }}
+        </div>
+    </div>
+
+    <div class="mb-4 flex justify-between items-center text-sm text-slate-500">
+        <div class="flex space-x-4">
+            <div>Company Name</div>
+            <div>{{ $job->location }}</div>
+        </div>
+        <div class="flex space-x-1 text-xs">
+            <x-tag>{{ Str::ucfirst($job->experience) }}</x-tag>
+            <x-tag>{{ $job->category }}</x-tag>
+        </div>
+    </div>
+
+    <p class="text-sm text-slate-500 mb-4">{!! nl2br(__($job->description)) !!}</p>
+
+    {{ $slot }}
+</x-card>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', fn() => to_route('jobs.index'));
-Route::resource('jobs', JobController::class)->only(['index']);
+Route::resource('jobs', JobController::class)->only(['index', 'show']);
