654806c08f85a6885ceb5cc7603a5ad1aaa95656
Conor <conor@visibleone.com>
2023-06-26 18:45:31 +0800

    Filtering Jobs: Min & Max Salary :white_check_mark:

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -14,11 +14,27 @@ class JobController extends Controller
     {
         $jobs = Job::query();
 
-        $jobs->when(request('search'), function ($query) {
-            $query
-                ->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('description', 'like', '%' . request('search') . '%');
-        });
+        $jobs
+            ->when(
+                request('search'),
+                function ($query) {
+                    $query->where(function ($query) {
+                        $query
+                            ->where('title', 'like', '%' . request('search') . '%')
+                            ->orWhere('description', 'like', '%' . request('search') . '%');
+                    });
+                }
+            )->when(
+                request('min_salary'),
+                function ($query) {
+                    $query->where('salary', '>=', request('min_salary'));
+                }
+            )->when(
+                request('max_salary'),
+                function ($query) {
+                    $query->where('salary', '<=', request('max_salary'));
+                }
+            );
 
         return view('job.index', ['jobs' => $jobs->get()]);
     }
