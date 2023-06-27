389c874f0f5f53abc0b17449be15a360e4e8595a
Conor <conor@visibleone.com>
2023-06-27 10:56:45 +0800

    Refactor Time: Plain Vanilla Javascript to Alpine.js :pushpin:

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,7 +1,10 @@
 <div class="relative">
-    @if ($formId)
+    @if ($formRef)
         <button type="button" class="absolute top-0 right-0 flex h-full items-center pr-2"
-            onclick="document.getElementById('{{ $name }}').value = ''; document.getElementById('{{ $formId }}').submit();">
+            @click="
+                $refs['input-{{ $name }}'].value = '';
+                $refs['{{ $formRef }}'].submit();
+            ">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-4 h-4 text-slate-500">
                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -9,7 +12,7 @@
             </svg>
         </button>
     @endif
-    <input type="text" name="{{ $name }}" value="{{ $value }}" placeholder="{{ $placeholder }}"
-        id="{{ $name }}"
+    <input x-ref="input-{{ $name }}" type="text" name="{{ $name }}" value="{{ $value }}"
+        placeholder="{{ $placeholder }}" id="{{ $name }}"
         class="w-full rounded-md border-0 py-1.5 px-2.5 pr-8 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2" />
 </div>
