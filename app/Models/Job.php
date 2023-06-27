be91ede8cbd6d46c1a9e98f42ce303d94e836ea5
Conor <conor@visibleone.com>
2023-06-27 11:33:29 +0800

    Refactor Time: Refactoring Filters Backend Logic :bookmark:

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Job extends Model
 {
@@ -12,4 +14,37 @@ class Job extends Model
     public static array $experience = ['entry', 'intermediate', 'senior'];
 
     public static array $category = ['IT', 'Marketing', 'Finance', 'Sales', 'Customer Support'];
+
+    public function scopeFilter(Builder | QueryBuilder $query, array $filters): Builder | QueryBuilder
+    {
+
+        return $query->when(
+            $filters['search'] ?? null,
+            function ($query, $search) {
+                $query->where(function ($query) use ($search) {
+                    $query
+                        ->where('title', 'like', '%' . $search . '%')
+                        ->orWhere('description', 'like', '%' . $search . '%');
+                });
+            }
+        )
+            ->when(
+                $filters['min_salary'] ?? null,
+                function ($query, $minSalary) {
+                    $query->where('salary', '>=', $minSalary);
+                }
+            )
+            ->when(
+                $filters['max_salary'] ?? null,
+                function ($query, $maxSalary) {
+                    $query->where('salary', '<=', $maxSalary);
+                }
+            )
+            ->when($filters['experience'] ?? null, function ($query, $experience) {
+                $query->where('experience', $experience);
+            })
+            ->when($filters['category'] ?? null, function ($query, $category) {
+                $query->where('category', $category);
+            });
+    }
 }
