e202de3c469b051b8bccc2a892175f5b06a6a9e1
Conor <conor@visibleone.com>
2023-06-27 16:30:57 +0800

    Applying for Jobs: Controller, Routing and Application Form :construction:

---
+++ app/Http/Controllers/JobApplicationController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobApplicationController extends Controller
+{
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Job $job)
+    {
+        return view('job_application.create', ['job' => $job]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,7 @@ Route::get('/login', fn () => to_route('auth.create'))->name('login');
 Route::resource('auth', AuthController::class)->only(['create', 'store']);
 Route::delete('logout', fn () => to_route('auth.destroy'))->name('logout');
 Route::delete('auth', [AuthController::class, 'destroy'])->name('auth.destroy');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('jobs.applications', JobApplicationController::class)->only(['create', 'store']);
+});
