31b087772d33504450440e07ac62a4bbf3ea1cc2
Conor <conor@visibleone.com>
2023-06-27 11:51:39 +0800

    Employer: Model, Migration and Relation :bookmark:

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Job extends Model
@@ -14,6 +15,11 @@ class Job extends Model
     public static array $experience = ['entry', 'intermediate', 'senior'];
 
     public static array $category = ['IT', 'Marketing', 'Finance', 'Sales', 'Customer Support'];
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 
     public function scopeFilter(Builder | QueryBuilder $query, array $filters): Builder | QueryBuilder
     {
