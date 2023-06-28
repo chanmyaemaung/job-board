a7e516b71e4d5204da5b9425149a8595fac84a1c
Conor <conor@visibleone.com>
2023-06-28 12:01:26 +0800

    Refactor: Show error flash message at layout :white_check_mark:

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -42,12 +42,21 @@
         </ul>
     </nav>
 
-    <!-- Flash Message -->
+    <!-- Flash Success Message -->
     @if (session('success'))
         <div role="alert"
             class="my-8 rounded-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
             <p class="font-bold">Success!</p>
             <p>{{ session('success') }}</p>
+        </div>
+    @endif
+
+    <!-- Flash Error Message -->
+    @if (session('error'))
+        <div role="alert"
+            class="my-8 rounded-md border-l-4 border-red-300 bg-red-100 p-4 text-red-700 opacity-75">
+            <p class="font-bold">Error!</p>
+            <p>{{ session('error') }}</p>
         </div>
     @endif
 
