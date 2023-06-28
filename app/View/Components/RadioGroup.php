f5234c0d84d3cf30e9eaa80d60006b676bbb8a55
Conor <conor@visibleone.com>
2023-06-28 13:32:45 +0800

    Employer: Adding Job Form🚧

--- app/View/Components/RadioGroup.php
+++ app/View/Components/RadioGroup.php
@@ -14,6 +14,8 @@ class RadioGroup extends Component
     public function __construct(
         public string $name,
         public array $options,
+        public ?bool $allOption = true,
+        public ?string $value = null,
     ) {
         //
     }
---
+++ resources/views/my_job/create.blade.php
@@ -0,0 +1,46 @@
+<x-layout>
+    <x-breadcrumbs :links="['My Jobs' => route('my-jobs.index'), 'Create' => '#']" class="mb-4" />
+
+    <x-card class="p-4 mb-8">
+        <form action="{{ route('my-jobs.store') }}" method="POST">
+            @csrf
+
+            <div class="mb-4 grid grid-cols-2 gap-4">
+                <div>
+                    <x-label for="title" :required="true" aria-required="true">Job Title</x-label>
+                    <x-text-input name="title" id="title" />
+                </div>
+
+                <div>
+                    <x-label for="location" :required="true" aria-required="true">Job Location</x-label>
+                    <x-text-input name="location" id="location" />
+                </div>
+
+                <div class="col-span-2">
+                    <x-label for="salary" :required="true" aria-required="true">Salary</x-label>
+                    <x-text-input name="salary" id="salary" type="number" />
+                </div>
+
+                <div class="col-span-2">
+                    <x-label for="description" :required="true" aria-required="true">Description</x-label>
+                    <x-text-input name="description" id="description" type="textarea" />
+                </div>
+
+                <div>
+                    <x-label for="experience" :required="true" aria-required="true">Experience</x-label>
+                    <x-radio-group name="experience" id="experience" :all-option="false" :value="old('experience')" :options="array_combine(
+                        array_map('ucfirst', \App\Models\Job::$experience),
+                        \App\Models\Job::$experience,
+                    )" />
+                </div>
+
+                <div>
+                    <x-label for="category" :required="true" aria-required="true">Category</x-label>
+                    <x-radio-group name="category" id="category" :all-option="false" :value="old('category')" :options="\App\Models\Job::$category" />
+                </div>
+            </div>
+
+            <x-button class="w-full">Create Job</x-button>
+        </form>
+    </x-card>
+</x-layout>
